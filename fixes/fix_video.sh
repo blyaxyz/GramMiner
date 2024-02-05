56f1129d66118f95c848b64eea8c6f806a07e0a2
blyaxyz <blyaxyz@gmail.com>
2024-02-05 18:29:43 +0300

    folder

---
+++ fixes/fix_video.sh
@@ -0,0 +1,4 @@
+sudo apt install -y nvidia-opencl-dev 
+sudo apt install -y nvidia-driver nvidia-cuda-toolkit nvidia-opencl-icd 
+sudo apt install -y nvidia-cuda-toolkit
+sudo apt install -y clinfo 
