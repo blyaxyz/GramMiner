e6f2b2dde65d42a87319d1302d9dead56bd5448e
blyaxyz <blyaxyz@gmail.com>
2024-02-06 15:32:18 +0300

    change to node 21 + small fixes

--- jupiterUbuntu.sh
+++ jupiterUbuntu.sh
@@ -1,6 +1,6 @@
 apt update
 apt install -y curl
 apt install -y fish
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash
+curl -fsSL https://deb.nodesource.com/setup_21.x | bash
 apt install -y nodejs
 apt install -y npm
